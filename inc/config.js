fb0d1d4eb28700b2353f074fe5258cc2d8f8ddec
Huynh Quoc Huy <huykyunh.k@gmail.com>
2025-06-10 10:05:12 +0700

    first commit

---
+++ inc/config.js
@@ -0,0 +1,16 @@
+import { initializeApp } from "https://www.gstatic.com/firebasejs/11.3.1/firebase-app.js";
+import { getAnalytics } from "https://www.gstatic.com/firebasejs/11.3.1/firebase-analytics.js";
+
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyDhTy2iASSLCAdo9ogwNCSDsZHPLvN86zA",
+  authDomain: "angiang-map.firebaseapp.com",
+  projectId: "angiang-map",
+  storageBucket: "angiang-map.firebasestorage.app",
+  messagingSenderId: "32261547754",
+  appId: "1:32261547754:web:309cb3dcde7cc1faf21993",
+  measurementId: "G-58QHVC3912"
+};
+
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
